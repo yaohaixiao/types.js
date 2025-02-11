29cf66a845c6176d23ad890e5f774f42e1fa2ff5
Yaohaixiao <yaohaixiao@gmail.com>
2025-02-11 09:03:33 +0800

    chore: 更新打包文件

--- types/isWindow.d.ts
+++ types/isWindow.d.ts
@@ -7,5 +7,24 @@ export default isWindow;
  * @category DOM
  * @param {*} val - 要检测的数据
  * @return {Boolean} 'val' 是 Window 对象，返回 true，否则返回 false
+ * @example
+ *
+ * const $list = document.getElementById('list')
+ *
+ * Types.is($list) // -> 'element'
+ * Types.isWindow($list) // -> false
+ *
+ * Types.isWindow(window) // -> true
+ *
+ * const nonWindowObject = {
+ *   // 模拟部分 Window 对象的属性，但不完整
+ *   document: {},
+ *   location: {},
+ *   // 缺少 alert 和 setInterval 属性
+ * }
+ *
+ * Types.isWindow(nonWindowObject) // -> false
+ *
+ * Types.isWindow(null) // -> false
  */
 declare function isWindow(val: any): boolean;
