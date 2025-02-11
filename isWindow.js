d80d0992f43ff232fe39e6c4777afebe799cd1ee
Yaohaixiao <yaohaixiao@gmail.com>
2025-02-11 09:01:47 +0800

    feat: 添加 isWindow() 方法；

---
+++ isWindow.js
@@ -0,0 +1,33 @@
+/**
+ * 检测测试数据是否为 Window 对象
+ * ========================================================================
+ * @method isWindow
+ * @since 2.7.0
+ * @category DOM
+ * @param {*} val - 要检测的数据
+ * @return {Boolean} 'val' 是 Window 对象，返回 true，否则返回 false
+ * @example
+ *
+ * const $list = document.getElementById('list')
+ *
+ * Types.is($list) // -> 'element'
+ * Types.isWindow($list) // -> false
+ *
+ * Types.isWindow(window) // -> true
+ *
+ * const nonWindowObject = {
+ *   // 模拟部分 Window 对象的属性，但不完整
+ *   document: {},
+ *   location: {},
+ *   // 缺少 alert 和 setInterval 属性
+ * }
+ *
+ * Types.isWindow(nonWindowObject) // -> false
+ *
+ * Types.isWindow(null) // -> false
+ */
+const isWindow = (val) => {
+  return !!(val && val.document && val.location && val.alert && val.setInterval)
+}
+
+export default isWindow
